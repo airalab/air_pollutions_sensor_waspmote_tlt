3deb355f93a40968a9402cdddd04ef04cb9417ac
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-03-05 14:04:41 +0400

    Nix build files

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ stdenv
+, python3Packages
+, mkRosPackage
+, robonomics_comm 
+}:
+
+mkRosPackage rec {
+  name = "${pname}-${version}";
+  pname = "air_pollutions_sensor_waspmote_tlt";
+  version = "master";
+
+  src = ./.;
+
+  propagatedBuildInputs = [
+    python3Packages.ed25519
+    robonomics_comm
+  ];
+
+  meta = with stdenv.lib; {
+    description = "air_pollutions_sensor_waspmote_tlt package";
+    homepage = http://github.com/airalab/air_pollutions_sensor_waspmote_tlt;
+    license = licenses.bsd3;
+    maintainers = [ "Alisher Khassanov <alisher@aira.life>" ];
+  };
+}
